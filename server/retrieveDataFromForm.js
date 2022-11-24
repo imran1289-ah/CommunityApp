b5fa84ba2b598848bde6b55625a317d491b1a4ea
RandomKingRD <rohandas@live.ca>
2022-11-24 15:40:40 -0500

    Create retrieveDataFromForm.js

---
+++ server/retrieveDataFromForm.js
@@ -0,0 +1,9 @@
+const first_name = document.getElementById("first_name").value;
+const last_name = document.getElementById("last_name").value;
+const age = document.getElementById("age").value;
+const job_type = document.getElementById("job_type").value;
+
+function sendDataToEmployer() {
+
+    print(first_name, last_name, age, job_type);
+}

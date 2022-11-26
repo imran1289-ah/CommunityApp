6b9d938a51a701096303cd2ea8132973e6450278
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2022-11-26 15:43:40 -0500

    Add a prototype for sequence diagram

--- server/login.js
+++ server/login.js
@@ -3,15 +3,21 @@ const password = document.getElementById("passowrd").value;
 const message = document.getElementById("message");
 
 function login() {
-  if (username == "imran" && password == "soen343") {
-    document.getElementById("message").value = "Welcome back Imran !!!";
-  } else if ((username = "employer" && password == "soen343")) {
-    document.getElementById("message").value = "Welcome back Employer !!!";
-    localStorage.setItem("employerLoggedIn", 1);
-  } else if (username == "" || password == "") {
-    document.getElementById("message").value =
+  if (username === "imran" && password === "soen343") {
+    document.getElementById("message").innerHTML = "Welcome back Imran !!!";
+    localStorage.setItem("LoggedIn", 1);
+  } else if (username === "employer" && password === "soen343") {
+    document.getElementById("message").innerHTML = "Welcome back Employer !!!";
+    localStorage.setItem("LoggedIn", 2);
+  } else if (username === "" || password === "") {
+    document.getElementById("message").innerHTML =
       "Login unsucesfull. Please try again";
   } else {
-    document.getElementById("message").value = "Wrong Credentials";
+    document.getElementById("message").innerHTML = "Wrong Credentials";
   }
+}
+
+function logout() {
+  document.getElementById("message").innerHTML = "Logged Out";
+  localStorage.setItem("LoggedIn", 0);
 }

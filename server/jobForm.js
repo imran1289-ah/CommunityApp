c74983d9fef9ecf1d6f02f9b39e45cc999256a21
Kunal Shah <37922021+Kunal22shah@users.noreply.github.com>
2022-11-26 12:27:32 -0500

    reading form input and updating the json file

--- server/jobForm.js
+++
@@ -1,4 +0,0 @@
-fetch("http://127.0.0.1:5500/SOEN343_CommunityApp/jobForm.json").then((response) => response.json())
-    .then((json) => console.log(json));
-
-console
---
+++ server/jobForm.php
@@ -0,0 +1,28 @@
+<?php
+
+
+if(isset($_REQUEST['submit'])){
+
+    $data = "";
+    $filename = "../jobForm.json";
+
+
+    $data = file_get_contents($filename);
+
+
+
+$data_arr = json_decode($data,true);
+
+$data_arr["jobs"][] = array('fname' => $_REQUEST['fname'],'lname' => $_REQUEST['lname'],'email' => $_REQUEST['email'],'age' => $_REQUEST['age'],
+'jobTitle' => $_REQUEST['jobTitle'],'resume' => $_REQUEST['resume']);
+
+$newdata = json_encode($data_arr);
+file_put_contents($filename,$newdata);
+
+header("Location:http://localhost/server/test.html ");
+}
+
+
+
+
+?>

c62ee99f33fedb17f4c2b2a1b0c30cffc0f28a07
imran1289-ah <77070862+imran1289-ah@users.noreply.github.com>
2022-11-27 19:16:37 -0500

    final changes for sequence diagram implementation

--- server/retrieveData.js
+++ server/retrieveData.js
@@ -1,19 +1,15 @@
-// const response = await fetch("http://localhost/jobForm.json");
-
-// let resp = await response.json();
-
-// console.log(resp);
-
+//Javascript function to retrieve data from json file (WebDev Tutorials)
 fetch("http://localhost/jobForm.json")
-    .then(function(response) {
-        return response.json();
-
-    }).then(
-        function(jobs) {
-            let placeholder = document.querySelector("#data-output");
-            let out = "";
-            for (let job of jobs.jobs) {
-                out += `
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jobs) {
+    let jobsection = document.getElementById("jobApplications");
+    //Iterate over jobs in jobForm.json to get each application
+    for (let job of jobs.jobs) {
+      jobsection.innerHTML =
+        jobsection.innerHTML +
+        `
          <tr>
             <td>${job.fname} </td>
             <td>${job.lname}</td>
@@ -22,37 +18,9 @@ fetch("http://localhost/jobForm.json")
             <td>${job.jobTitle}</td>
             <td>${job.resume}</td>
             <td>
-          <button type="submit" name="submit" class="btn btn-primary">Approve</button>
-          <button type="submit" name="submit" class="btn btn-danger">Deny</button>
-          </td>
+                <button type="submit" name="submit" class="btn btn-primary">Approve</button>
+                <button type="submit" name="submit" class="btn btn-danger">Deny</button>
+            </td>
          </tr>`;
-            }
-
-            placeholder.innerHTML = out;
-        });
-
-// for (let i = 0; i < resp.jobs.length; i++) {
-//     let element = document.createElement("th");
-//     let fnamenode = document.createTextNode(resp.jobs[i].fname);
-//     let lnamenode = document.createTextNode(resp.jobs[i].lname);
-//     let emailnode = document.createTextNode(resp.jobs[i].email);
-//     let agenode = document.createTextNode(resp.jobs[i].age);
-//     let jobtitlenode = document.createTextNode(resp.jobs[i].jobTitle);
-//     let resumenode = document.createTextNode(resp.jobs[i].resume);
-
-
-//     element.appendChild(fnamenode);
-//     element.appendChild(lnamenode);
-//     element.appendChild(emailnode);
-//     element.appendChild(agenode);
-//     element.appendChild(jobtitlenode);
-//     element.appendChild(resumenode);
-
-//     document.getElementById("fname").appendChild(fnamenode);
-//     document.getElementById("lname").appendChild(lnamenode);
-//     document.getElementById("email").appendChild(emailnode);
-//     document.getElementById("age").appendChild(agenode);
-//     document.getElementById("jobTitle").appendChild(jobtitlenode);
-//     document.getElementById("resume").appendChild(resumenode);
-
-// }
+    }
+  });
